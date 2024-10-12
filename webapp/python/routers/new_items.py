3499e959ea1833731159b78120551f1b1d17c14b
sibapybot <sibabot.python@gmail.com>
2024-10-12 21:39:20 +0900

    a

--- webapp/python/routers/new_items.py
+++ webapp/python/routers/new_items.py
@@ -1,4 +1,26 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette.status import HTTP_403_FORBIDDEN, HTTP_400_BAD_REQUEST
+from database import get_db
 
 router = APIRouter()
+
+@router.get("/new_items")
+async def get_new_items(
+            item_id: int = 0,
+            created_at: int = 0,
+            db: Session = Depends(get_db)
+        ):
+
+    if not isinstance(item_id, int) or item_id <= 0:
+        raise HTTPException(
+        status_code=HTTP_400_BAD_REQUEST,
+        detail="item_id param error"
+    )
+    if not isinstance(item_id, int) or created_at <= 0:
+        raise HTTPException(
+        status_code=HTTP_400_BAD_REQUEST,
+        detail="created_at param error"
+    )
+
+    items = []
