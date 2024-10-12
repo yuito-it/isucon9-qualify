6b5b237fc5ca07c0e3dcb8b06e887a56128c3d11
sibapybot <sibabot.python@gmail.com>
2024-10-12 21:22:38 +0900

    database出来た

--- webapp/python/database.py
+++ webapp/python/database.py
@@ -6,9 +6,13 @@ from dotenv import load_dotenv
 
 # .envファイルの内容を読み込見込む
 load_dotenv(dotenv_path='../.env')
+host = os.getenv('MYSQL_HOST', '127.0.0.1')
+port = int(os.getenv('MYSQL_PORT', 3306))
+user = os.getenv('MYSQL_USER', 'isucari')
+password = os.getenv('MYSQL_PASS', 'isucari')
+db = os.getenv('MYSQL_DBNAME', 'isucari')
 
-
-SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{db}'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
