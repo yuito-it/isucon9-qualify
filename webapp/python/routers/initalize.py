994cb8e74ba5341b7db5abfe97aa04dd5011be90
YuitoAkatsuki <yuito@yuito.work>
2024-10-12 21:47:37 +0900

    feat: Refactor initialize endpoint to add configuration values

--- webapp/python/routers/initalize.py
+++ webapp/python/routers/initalize.py
@@ -1,17 +1,27 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from ..database import get_db
+from ..models import config
+
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/apps",
     tags=["apps"],
 )
 
-@router.post("/initialize", response_model=AppSchema)
-async def create_apps(
-            app: AppCreateSchema,
-            user: MeSchema = Depends(verify_token),
+class Item(BaseModel):
+    payment_service_url: str
+    shipment_service_url: str
+
+@router.post("/initialize")
+async def initalize(
+            item: Item,
             db: Session = Depends(get_db)
         ):
-    new_app: AppSchema = create_app(db=db, app=app)
-    app: AppSchema = add_admin_user_to_app(db, new_app.id, user.id)
-    return app
+    config = config(name="payment_service_url", value=item.payment_service_url)
+    db.add(config)
+    return {
+        "campaign": 0,
+        "language": "python"
+    }
